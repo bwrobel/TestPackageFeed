77268682a1e2fa86b5d298fcb5ce2e6943bf3db3
bwrobel@mediaingenuity.com <bwrobel@mediaingenuity.com>
2014-09-22 11:14:33 +0100

    Give Voice

---
+++ TestPackage/Tester.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestPackage
+{
+    public class Tester
+    {
+        public static void GiveVoice()
+        {
+            Console.WriteLine("Woof!!!");
+        }
+    }
+}
