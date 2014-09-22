9e1f914fababdab1a535c1b9da79c9761fd74ae7
bwrobel@mediaingenuity.com <bwrobel@mediaingenuity.com>
2014-09-22 19:36:15 +0100

    metadata information update

--- TestPackage/Properties/AssemblyInfo.cs
+++ TestPackage/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestPackage")]
 [assembly: AssemblyDescription("This is another attempt to hosting packages in MyGet")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Bartosz Wróbel")]
 [assembly: AssemblyProduct("TestPackage")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © Bartosz Wróbel 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.0.*")]
